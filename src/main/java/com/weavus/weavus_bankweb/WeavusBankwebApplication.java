4a3450f956277e1cda99317c86e3b3ca2797ea3b
DESKTOP-OJVF8KK\seol <tjfrhkdwls12@naver.com>
2025-07-04 14:42:45 +0900

    最初のCOMMIT

---
+++ src/main/java/com/weavus/weavus_bankweb/WeavusBankwebApplication.java
@@ -0,0 +1,13 @@
+package com.weavus.weavus_bankweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavusBankwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeavusBankwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weavus/weavus_bankweb/WeavusBankwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weavus.weavus_bankweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavusBankwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
