94d4e2af6d5358f2971f3d44dd8654b9041b0042
DESKTOP-OJVF8KK\seol <tjfrhkdwls12@naver.com>
2025-08-19 09:54:55 +0900

    細かい部分を修正しました。

--- src/test/java/com/weavus/weavus_bankweb/transactionTest/AccountTest.java
+++ src/test/java/com/weavus/weavus_bankweb/transactionTest/AccountTest.java
@@ -42,10 +42,10 @@ public class AccountTest {
         System.out.println("LAE: " + lae.get(0).getAccount_number());
     }
 
-    @Test
+    //@Test
     //残高UPDATEテスト
-    void balanceTest(){
-        accountsInterface.updateBalance("123456789022", 20000);
-        assertEquals("残高が間違います。", 20000, accountsInterface.findBalanceByBalanceId("123456789022"));
-    }
+//    void balanceTest(){
+//        accountsInterface.updateBalance("123456789022", 20000);
+//        assertEquals("残高が間違います。", 20000, accountsInterface.findBalanceByBalanceId("123456789022"));
+//    }
 }
